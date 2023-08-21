df564a63d29790754182b3474d05cd7816c83abf
bgrowcott <bgrowcott@ucb.ac.uk>
2023-08-21 15:35:44 +0100

    added fontawesome

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,7 +1,15 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
+
 const Home = () => {
   return (
     <section>
-      <h2>Hello World</h2>
+      <h2>
+        {/* font awesome example */}
+        <FontAwesomeIcon className="me-1" icon={solid("building")} />
+        Hello World
+      </h2>
     </section>
   );
 }
